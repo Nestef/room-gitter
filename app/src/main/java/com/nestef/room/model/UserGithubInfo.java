99f17bcf5540e42a9d8fb9e0f16db60f0eb0fdcf
Noah Steffes <nestef@protonmail.com>
2018-04-18 13:59:08 -0500

    add additional models

---
+++ app/src/main/java/com/nestef/room/model/UserGithubInfo.java
@@ -0,0 +1,10 @@
+package com.nestef.room.model;
+
+/**
+ * Created by Noah Steffes on 4/18/18.
+ */
+public class UserGithubInfo {
+    int followers;
+    int public_repos;
+    int following;
+}
---
+++ app/src/main/java/com/nestef/room/model/UserProfile.java
@@ -0,0 +1,19 @@
+package com.nestef.room.model;
+
+/**
+ * Created by Noah Steffes on 4/18/18.
+ */
+public class UserProfile {
+    String id;
+    String username;
+    String displayname;
+    boolean invited;
+    boolean has_gitter_login;
+    String company;
+    String location;
+    String email;
+    String website;
+    String profile;
+    UserGithubInfo github;
+    String gravatarImageUrl;
+}
