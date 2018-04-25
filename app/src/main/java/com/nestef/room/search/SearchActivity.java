bee7e7499d8b5f1b4c81803423c1513827b79018
Noah Steffes <nestef@protonmail.com>
2018-04-25 11:14:35 -0500

    add search activity

---
+++ app/src/main/java/com/nestef/room/search/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.nestef.room.search;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Noah Steffes on 4/24/18.
+ */
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
