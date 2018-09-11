f84b22b1ff8401df531d30a3fb217ad210a64d10
Noah Steffes <nestef@protonmail.com>
2018-09-11 15:31:02 -0500

    change search presenter callbacks

--- app/src/main/java/com/nestef/room/data/SearchManager.java
+++ app/src/main/java/com/nestef/room/data/SearchManager.java
@@ -7,7 +7,9 @@ import com.nestef.room.services.GitterServiceFactory;
 
 import java.util.List;
 
+import retrofit2.Call;
 import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by Noah Steffes on 7/3/18.
@@ -20,12 +22,32 @@ public class SearchManager {
         mService = GitterServiceFactory.makeApiService(prefManager.getAuthToken());
     }
 
-    public void fetchRoomSuggestions(Callback<List<Room>> callback) {
-        mService.getSuggestedRooms().enqueue(callback);
+    public void fetchRoomSuggestions(SearchCallback callback) {
+        mService.getSuggestedRooms().enqueue(new Callback<List<Room>>() {
+            @Override
+            public void onResponse(Call<List<Room>> call, Response<List<Room>> response) {
+                callback.returnSuggestions(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<Room>> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
     }
 
-    public void searchRoomsByQuery(String query, Callback<QueryResponse> callback) {
-        mService.searchRooms(query).enqueue(callback);
+    public void searchRoomsByQuery(String query, SearchCallback callback) {
+        mService.searchRooms(query).enqueue(new Callback<QueryResponse>() {
+            @Override
+            public void onResponse(Call<QueryResponse> call, Response<QueryResponse> response) {
+                callback.returnSearchResults(response.body().results);
+            }
+
+            @Override
+            public void onFailure(Call<QueryResponse> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
     }
 
     public interface SearchCallback {
